e5ea4d307673f20e5ee03478c38434811ba404ea
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-25 23:15:09 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -8,3 +8,12 @@ module SongsHelper
     end
   end
 end
+
+
+# def artist_select(artist, song)
+#   if artist
+#     artist.name
+#   else
+#     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+#   end
+# end
